09c5569859c9ca7a1b07f76321b717931199f0ff
ritzy-git <riz64ritzy@gmail.com>
2019-07-15 11:49:58 +0530

    latest

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 const express = require("express"),
       app = express(),
       bodyparser = require('body-parser'),
-      db =  require('./src/db/db'),
-      server = require('http').Server(app),
-      io = require('socket.io').listen(app);
+      server = require('http').createServer(app),
+      io = require('socket.io')(server);
 
-
-
-server.
 app.use(bodyparser.json())
 
 const port = process.env.PORT || 3000;
 app.post('/Savedata',(req,res) => {
-    //console.log(req.body)
-    io.on('connection', (socket) => {
+    
+    io.on('connection', (socket,req) => {
         console.log(req.body);
         socket.emit('location',req.body);
-        //socket.on('my other event', function (data) {
-        //    console.log(data);
-        //  });
+        console.log(req.body);
     })
-
-        
-    
-    
-
 })
 
-app.listen(port, () => 
-    console.log(`Server is running on port ${port}`)
-)
+io.on('connection', () =>{
+    console.log('a user is connected')
+  })
+
+
+
+server.listen(port);
+// app.listen(port, () => 
+//     console.log(`Server is running on port ${port}`)
+// )
 
 
--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose() // verbose to get debug logs
 const path = require('path')
-//var io = require('socket.io')(80);
+var io = require('socket.io')(80);
 
 
 let db = new sqlite3.Database(path.join(__dirname, '../../db/mydata.db'), sqlite3.OPEN_READWRITE, (err) => {
@@ -14,17 +14,16 @@ let db = new sqlite3.Database(path.join(__dirname, '../../db/mydata.db'), sqlite
 
 exports.insertdata = function () {
     
-    //io.on('connection', function (socket) {
-    var socket = io.connect('http://localhost:3001');
-    socket.on('location', function (data) {
-        console.log(data);
-        db.run(`INSERT INTO location(lat,long,phoneno,detectiontime) VALUES(?,?,?,?)`, [data.Location["Latitude"], data.Location["longitude"], data.phone_number, data.detection_time], function (err) {
-            if (err) {
-                return console.log(err.message);
-            }
-        }, () => { db.close() });
-    });
-    //})
+    //io.on('connection', function(socket){
+        var socket = io.connect('http://localhost:3000');
+        socket.on('location', function(msg){
+            db.run(`INSERT INTO location(lat,long,phoneno,detectiontime) VALUES(?,?,?,?)`, [data.Location["Latitude"], data.Location["longitude"], data.phone_number, data.detection_time], function (err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            }, () => { db.close() });
+        });
+    //});
 }
 exports.showdata = function (callback) {
 
